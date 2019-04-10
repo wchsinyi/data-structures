184fa79356826417e2b0d50c4ce4dcf68e9d8d38
Bill Lou <billloukehao@gmail.com>
2019-04-10 14:53:49 -0700

    Implement working es6 queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.start = 0;
+    this.end = 0;
   }
 
+  enqueue(val) {
+    this.storage[this.end++] = val;
+  }
+
+  dequeue() {
+    if (this.end > this.start) {
+      return this.storage[this.start++];
+    }
+  }
+
+  size() {
+    return this.end - this.start;
+  }
 }
