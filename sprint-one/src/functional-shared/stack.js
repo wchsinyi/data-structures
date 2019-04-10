a6328ea1f106337078bcf457a70bd19abc980689
Bill Lou <billloukehao@gmail.com>
2019-04-10 12:17:09 -0700

    Refactored to be more DRY

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,11 +2,9 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var instance = {};
-  var storage = {};
-  var amount = 0;
+  instance.storage = {};
+  instance.amount = 0;
 
-  instance.storage = storage;
-  instance.amount = amount;
   _.extend(instance, stackMethods);
   return instance;
 };
