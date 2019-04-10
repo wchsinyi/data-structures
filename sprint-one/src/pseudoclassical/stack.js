484599f6112ceb29abb78a933e1750197fc70131
Bill Lou <billloukehao@gmail.com>
2019-04-10 14:45:52 -0700

    Implement working pseudoclassical stack function object

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.amount = 0;
 };
 
-
+Stack.prototype.push = function(val) {
+  this.amount++;
+  this.storage[this.amount] = val;
+}
+Stack.prototype.pop = function() {
+  if (this.amount > 0) {
+    return this.storage[this.amount--];
+  }
+}
+Stack.prototype.size = function() {
+  return this.amount;
+}
