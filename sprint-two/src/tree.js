4eca43dcb30e9d6fb9a476b737b27956b30ecc0f
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-11 11:17:18 -0700

    Complete tree function object

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,31 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  if (this.value === target) {
+    return true;
+  }
+  var isTrue = false;
+
+  for (let i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      isTrue = true;
+    }
+  }
+  return isTrue;
 };
 
 
