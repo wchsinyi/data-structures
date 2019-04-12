f05d6e0bd8e4fdf7302f7f350575c985b3b349ba
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-11 20:55:34 -0700

    Implement partial hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,32 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._kList.push(k)
+  if (!this._kList.includes(k) && this._storage[index] != undefined) {
+    // handle functional conflict 
+  } else {
+    // set the index'th position of the storage array to v 
+    this._storage.set(index, v);
+  }
+
+
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var retrieveItem = this._storage.get(index);
+  return retrieveItem;
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
