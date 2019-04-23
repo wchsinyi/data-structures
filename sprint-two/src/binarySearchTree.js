02aa36ef3654c5897f302ac61bac6ee5cb94a18e
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-23 14:28:50 -0700

    Draft inorder traversal

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -56,3 +56,14 @@ var BinarySearchTree = function (value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var t = BinarySearchTree(1);
+
+var arr = [14,3,7,4,5,15,6,13,10,11,2,12,8,9];
+for (var i = 0; i < arr.length; i++) {
+  t.insert(arr[i]);
+}
+
+
+// https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
